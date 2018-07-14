33859b442948959f4472902393f3e3c6687db195
Teerachart Jongkoldee <touchcAstle@Teeracharts-MacBook-Pro.local>
2018-07-14 23:09:00 +0700

    multiple ID

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+require('dotenv').config({})
 const momoka = require('momoka-core-bot')
+
 
 const strategies = require('./strategy/index')
 const _config = require('./config')
 
 momoka(_config, strategies).then((bot) => {
   bot.start()
+  bot.addLineBot('/line2', {
+    ..._config,
+    line: {
+      id: process.env.LINE_CHANNEL_ID_2,
+      secret: process.env.LINE_CHANNEL_SECRET_2,
+      token: process.env.LINE_CHANNEL_TOKEN_2
+    }
+  })
 })
