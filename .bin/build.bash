d399145ac606d9c2285df52312c47e1c90fb9e99
Rungsikorn R <rungsikorn.r@kbtg.tech>
2021-01-12 15:45:10 +0700

    update build-and-deploy script

--- .bin/build.bash
+++ .bin/build.bash
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+set -e
 rm -rf ./dist
 mkdir -p dist
 tar --exclude='.git' --exclude='.env' --exclude='./dist/artifact.tar' --exclude='./.db' --exclude='./node_modules' --exclude='./.vscode' -czf ./dist/artifact.tar ./
--- .bin/deploy.bash
+++ .bin/deploy.bash
@@ -1,4 +1,10 @@
-export $(cat .env | grep -v ^# | xargs)
+#!/usr/bin/env bash
+set -e
+
+[ -z $DEPLOY_TARGET ] && echo "Deploy target: " && read DEPLOY_TARGET
+
 echo 'Deploy Metano to '$DEPLOY_TARGET
 rsync -avz ./dist/artifact.tar $DEPLOY_TARGET:/root/metano-release.tar
+
+echo 'Reload Metano server'
 ssh -t $DEPLOY_TARGET 'cd /root && mkdir -p metano && tar -xvf metano-release.tar -C /root/metano && cd metano && npm install'
